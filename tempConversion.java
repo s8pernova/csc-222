9e5fabc0a242d5aa950e16fdf04132ea57d1ccc8
Aidan Hoo <achrunaway@gmail.com>
2025-09-06 12:38:12 -0400

    push

---
+++ tempConversion.java
@@ -0,0 +1,9 @@
+// Test for csc 222
+// Author: Aidan Hoo
+// Date: 9/9/25
+
+public class tempConversion {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
